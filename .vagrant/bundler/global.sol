3ed39c896dd6746a4684ccbc9672a88ef0a0f2a7
raulhprimo <raul.primoimp@gmail.com>
2024-04-02 22:08:00 -0300

    first commit

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.32.0"]]],"checksum":"8f8c082292f0c845948051e063125492a5a922fd2bca8a3fb7199a9f9d04e8fe","vagrant_version":"2.4.0"}
